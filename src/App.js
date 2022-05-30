660be1a11a0c9c6b96747f414568b6d13db75f16
Emanuel Gomila <gomilaemanuel@gmail.com>
2022-05-30 12:10:06 -0300

    comenzando a crear la pagina

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Componente from "./components/Componente";
-import Contador from "./components/Contador";
-const App = () => {
-  return (
+
+function App(){
+  return(
     <div>
-      <h1>Hola Mundo</h1>
-      <Componente msg="Hola Soy un componente funcional"/>
-      <Contador/>
+      
     </div>
   );
 }
